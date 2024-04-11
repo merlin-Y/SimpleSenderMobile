7f46788b34af3c9176d6f4778f437d3261a75cc6
merlin <1411194058@qq.com>
2024-04-11 17:31:24 +0800

    flush

--- app/src/main/java/cn/merlin/simplesendermobile/bean/File.kt
+++
@@ -1,8 +0,0 @@
-package cn.merlin.simplesendermobile.bean
-
-import java.io.Serializable
-
-data class File(
-    var fileName: String = "",
-    var totalPackets: Int = -1
-): Serializable
--- app/src/main/java/cn/merlin/simplesendermobile/bean/Request.kt
+++
@@ -1,9 +0,0 @@
-package cn.merlin.simplesendermobile.bean
-
-import java.io.Serializable
-
-class Request(
-    val requestCode: Int = -1,
-    val totalPackets: Int = -1,
-    val requestBody: Device = Device()
-): Serializable
